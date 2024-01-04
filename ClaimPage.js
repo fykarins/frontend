10cc3b9c2239600e3ccb2314ea62e141ed04a1f6
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-01-04 16:08:09 +0700

    Claim Pages

---
+++ ClaimPage.js
@@ -0,0 +1,269 @@
+import React, { useEffect, useState } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardHeaderToolbar,
+} from "../../../../_metronic/_partials/controls";
+
+import { ClaimTable } from "./ClaimTable";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  resetData,
+  selectClaim,
+  selectLoading,
+  selectPageNo,
+  selectPageSize,
+  selectTotalRecord,
+  fetchClaim,
+} from "./claimSlice";
+import { LayoutSplashScreen } from "../../../../_metronic/layout";
+import { showErrorDialog } from "../../../utility";
+import { selectUser } from "../../../modules/Auth/_redux/authRedux";
+import {
+  fetchBispar,
+  selectBispar,
+} from "../../administration/bussiness-parameter/parameter/parameterSlice";
+import Select from "react-select";
+import LoadingFetchData from "../../../utility/LoadingFetchData";
+
+export const ClaimPage = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const data = useSelector(selectClaim);
+  const dataBispar = useSelector(selectBispar);
+  const user = useSelector(selectUser);
+  const loading = useSelector(selectLoading);
+  const pageNo = useSelector(selectPageNo);
+  const pageSize = useSelector(selectPageSize);
+  const [overlayLoading, setOverlayLoading] = useState(false);
+  const totalRecord = useSelector(selectTotalRecord);
+
+  // Filter
+  const [vendorCode, setVendorCode] = useState("");
+  const [type, setType] = useState("");
+
+  const filterVendorCode =
+    user.vendor_code === null ? vendorCode : user.vendor_code;
+
+  useEffect(() => {
+    // Reset on first load
+    dispatch(resetData());
+
+    dispatch(
+      fetchBispar({
+        paramgroup_code: "claim_type",
+        pageNo: 1,
+        pageSize: -1,
+      })
+    );
+  }, [dispatch]);
+
+  const handleSearch = async () => {
+    const params = {
+      LFNR1: filterVendorCode,
+      pageNo: 1,
+      pageSize: 10,
+    };
+    try {
+      const response = await dispatch(fetchClaim(params));
+      if (response.payload.data.status === 200) {
+        setOverlayLoading(false);
+      } else if (
+        response.payload.data.error === "10008" ||
+        response.payload.data.error === "10009"
+      ) {
+        const action = await showErrorDialog(response.payload.data.message);
+        if (action.isConfirmed) await history.push("/logout");
+      } else {
+        showErrorDialog(response.payload.data.message);
+        setOverlayLoading(false);
+      }
+    } catch (error) {
+      showErrorDialog(error.message);
+      setOverlayLoading(false);
+    }
+  };
+
+  const handleTableChange = async (
+    type,
+    { page, sizePerPage, sortField, sortOrder, data }
+  ) => {
+    if (type === "pagination") {
+      const params = {
+        pageNo: page,
+        pageSize: sizePerPage,
+      };
+      try {
+        const response = await dispatch(fetchClaim(params));
+        if (response.payload.data.status === 200) {
+          setOverlayLoading(false);
+        } else if (
+          response.payload.data.error === "10008" ||
+          response.payload.data.error === "10009"
+        ) {
+          const action = await showErrorDialog(response.payload.data.message);
+          if (action.isConfirmed) await history.push("/logout");
+        } else {
+          showErrorDialog(response.payload.data.message);
+          setOverlayLoading(false);
+        }
+      } catch (error) {
+        showErrorDialog(error.message);
+        setOverlayLoading(false);
+      }
+    } else {
+      let result;
+      if (sortOrder === "asc") {
+        result = data.sort((a, b) => {
+          if (a[sortField] > b[sortField]) {
+            return 1;
+          } else if (b[sortField] > a[sortField]) {
+            return -1;
+          }
+          return 0;
+        });
+        console.log(result, "asc");
+      } else {
+        result = data.sort((a, b) => {
+          if (a[sortField] > b[sortField]) {
+            return -1;
+          } else if (b[sortField] > a[sortField]) {
+            return 1;
+          }
+          return 0;
+        });
+        console.log(result, "desc");
+      }
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  const typeOptions = dataBispar.map((e) => {
+    return {
+      value: e.param_code,
+      label: e.param_value,
+    };
+  });
+
+  const getValueType = (value, options) => {
+    const return_value = options.filter((val) => value === val.value);
+    return return_value;
+  };
+
+  const handleTypeChange = (e) => {
+    if (e === null) {
+      e = undefined;
+      setType("");
+    } else {
+      setType(e.value);
+    }
+  };
+
+  return loading ? (
+    <LayoutSplashScreen />
+  ) : (
+    <Card>
+      <CardHeader title="Claim">
+        {user.vendor_code === null && (
+          <CardHeaderToolbar>
+            <Button
+              className="btn btn-danger"
+              onClick={() => history.push("/transaction/claim/create")}
+            >
+              Create
+            </Button>
+          </CardHeaderToolbar>
+        )}
+      </CardHeader>
+      <LoadingFetchData active={overlayLoading} />
+      <CardBody>
+        {/* Filter */}
+        <Form className="mb-5">
+          <Form.Group as={Row}>
+            <Col sm={6}>
+              {user.vendor_code !== null && (
+                <Form.Group as={Row}>
+                  <Form.Label column sm={3}>
+                    <b>Vendor</b>
+                  </Form.Label>
+                  <Col sm={6}>
+                    <Form.Control
+                      type="text"
+                      placeholder="Vendor"
+                      value={user.vendor_name}
+                      disabled
+                    />
+                  </Col>
+                </Form.Group>
+              )}
+              {user.vendor_code === null && (
+                <Form.Group as={Row}>
+                  <Form.Label column sm={3}>
+                    <b>Vendor </b>
+                  </Form.Label>
+                  <Col sm={6}>
+                    <Form.Control
+                      type="text"
+                      placeholder="Vendor"
+                      onChange={(e) => {
+                        setVendorCode(e.target.value);
+                      }}
+                      value={vendorCode}
+                      onKeyPress={handleKeyPress}
+                    />
+                  </Col>
+                </Form.Group>
+              )}
+            </Col>
+
+            {/* Right Row */}
+
+            <Col sm={6}>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  <b>Type</b>
+                </Form.Label>
+                <Col sm={5}>
+                  <Select
+                    isClearable={true}
+                    options={typeOptions}
+                    value={getValueType(type, typeOptions)}
+                    placeholder="Select TYpe"
+                    onChange={handleTypeChange}
+                  />
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Col sm={3}>
+                  <Button className="btn btn-danger" onClick={handleSearch}>
+                    Search
+                  </Button>
+                </Col>
+              </Form.Group>
+            </Col>
+          </Form.Group>
+        </Form>
+
+        {/* Table */}
+        {data && data.length > 0 && (
+          <ClaimTable
+            data={data}
+            page={pageNo}
+            sizePerPage={pageSize}
+            totalSize={totalRecord}
+            onTableChange={handleTableChange}
+            loading={loading}
+          />
+        )}
+      </CardBody>
+    </Card>
+  );
+};
